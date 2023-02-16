0db3ea160d6d0dded6287639ae6d94a982cfcd95
w-nn <winn.memonotes@gmail.com>
2023-02-16 15:41:43 -0800

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,7 @@
  * main - A C program that prints the given text using function printf
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
