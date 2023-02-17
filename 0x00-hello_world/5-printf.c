d20b8b0241d025f898093dcc4025e59f61207470
w-nn <winn.memonotes@gmail.com>
2023-02-16 16:23:37 -0800

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/5-printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - A C program that prints the given text using function printf
- * Return: 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-A C program that prints given text using function printf, followed by a new line
+A C program that prints the size of various types on the computer it is compiled and run on
