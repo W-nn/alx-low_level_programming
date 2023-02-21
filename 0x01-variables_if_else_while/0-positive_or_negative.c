200aa4fb038b9429a24bb9e5de2a72e8119f6d80
w-nn <winn.memonotes@gmail.com>
2023-02-20 16:23:26 -0800

    Task 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * Print if number in the variable n is positive or negative
+ * Description: Print if number in the variable n is positive or negative
  * Return: Always 0
  */
 int main(void)
