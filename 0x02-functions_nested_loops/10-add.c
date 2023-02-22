25ce7fef1a12f451da5afa5fd79ccfa870497b85
w-nn <winn.memonotes@gmail.com>
2023-02-22 12:37:17 -0800

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers and returns the result
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
