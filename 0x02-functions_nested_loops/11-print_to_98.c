c7c68998f34a9aa62dd9a76c6272036f40c8935f
w-nn <winn.memonotes@gmail.com>
2023-02-22 14:29:00 -0800

    Task 11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,16 @@
 
 void print_to_98(int n)
 {
-	int g, h;
-
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (g = n; g <= n; g++)
-		{
-			if (g != 98)
-				printf("%d, ", g);
-			else if (g == 98)
-				printf("%d\n", g);
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	else if (n >= 98)
+	else
 	{
-		for (h = n; h >= n; h--)
-		{
-			if (h != 98)
-				printf("%d, ", h);
-			else if (h == 98)
-				printf("%d\n", h);
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
