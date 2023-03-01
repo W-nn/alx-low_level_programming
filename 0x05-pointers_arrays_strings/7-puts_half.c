fa24e46765cecfde5b2979034d043428669ecb76
w-nn <winn.memonotes@gmail.com>
2023-02-28 16:53:46 -0800

    Task 7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,27 +3,28 @@
 /**
  * puts_half - a function that prints half of a string,
  * followed by a new line
+ * If the number of characters is odd, the function should
+ * print the last n,  where n = (length_of_the_string - 1) / 2
  * @str: string to be printed
- * Return: nothing on success
+ * Return: half of string
  */
 
 void puts_half(char *str)
 {
-	int count = 0, i;
+	int a, n, longi;
 
-	while (str)
-	{
-		count++;
-		str++;
-	}
+	longi = 0;
 
-	for (i = 0; i < count; i++)
-		str--;
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
 
-	i = (count % 2 == 0) ? count / 2 : (count + 1) / 2;
+	n = (longi / 2);
 
-	for (; i < count; i++)
-		_putchar(str[i]);
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 
 	_putchar('\n');
 }
