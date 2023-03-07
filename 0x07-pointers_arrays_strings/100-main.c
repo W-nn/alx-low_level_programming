441ad42b3229b701a24709a19162c5c76cf52d87
w-nn <winn.memonotes@gmail.com>
2023-03-06 17:05:25 -0800

    Task 8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -15,6 +15,5 @@ int main(void)
 	printf("%s, %s\n", s0, s1);
 	set_string(&s1, s0);
 	printf("%s, %s\n", s0, s1);
-
 	return (0);
 }
--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * set_string - function that sets the value of a pointer to a char
