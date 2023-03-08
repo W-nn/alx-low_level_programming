08cdf036cb1582ff0f7f407a3308d934d5e15f8f
w-nn <winn.memonotes@gmail.com>
2023-03-08 15:38:02 -0800

    Task 6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-#include main.h"
+#include "main.h"
 
 int actual_prime(int n, int i);
 
@@ -14,7 +14,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n -1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
@@ -27,9 +27,9 @@ int is_prime_number(int n)
 
 int actual_prime(int n, int i)
 {
-	if (i ==1)
+	if (i == 1)
 		return (1);
-	if (n %i == 0 && i > 0)
+	if (n % i == 0 && i > 0)
 		return (0);
 	return (actual_prime(n, i - 1));
 }
